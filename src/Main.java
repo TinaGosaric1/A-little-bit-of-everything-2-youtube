cfde90f2be8a1f7de9618274bb6a56dca143e8ec
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-21 10:57:45 +0200

    Add input from file.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner scanner = new Scanner(new File("src/students.txt"));
+
+        List<String> students = new ArrayList<String>();
+
+        while(scanner.hasNextLine()){
+            students.add(scanner.nextLine());
+        }
+
+        for(String item : students){
+            System.out.println("Name: " + item);
+        }
+        scanner.close();
     }
 }
