73198814eb178158d9542c990de167db31f2ed14
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-24 12:21:30 +0200

    Using iterator in a list.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        //making stack from linked list
+
         try {
             Scanner scanner = new Scanner(new File("test.txt"));
         } catch (FileNotFoundException e) {
@@ -18,11 +15,11 @@ public class Main {
         names.push("Caleb"); // Inserts the element at the front of this list.
         names.push("Sue");
         names.push("Sally");
-        String firstName = names.pop();       //Removes and returns the first element of this list.
-        System.out.println(firstName);
 
-        while(!names.isEmpty()){
-            System.out.println(names.pop());
+        Iterator<String> it = names.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
         }
     }
 }
