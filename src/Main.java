dcdbc355f2fbde7dac8533d08cb57d1def8a2f96
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-25 10:11:33 +0200

    More on generic class.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Item<Person> item1 = new Item<>();
+        Pair<Person, Person> pair1 = new Pair<>();
         Person p1 = new Person("email@email.com", "G");
-        item1.setX(p1);
+        pair1.setX(p1);
+        pair1.setY(p1);
 
-        Person p2 = item1.getX();
+        Person p2 = pair1.getX();
+
+        Pair<Integer, String> pair2 = new Pair<>();
+        pair2.setX(12);
+        pair2.setY("My beautiful string.");
+
+        ArrayList<Pair<Integer, String>> pairs = new ArrayList<>();
+        pairs.add(pair2);
     }
 }
